cc9c2bc921c165cda6f95f559dbea4e38629dda0
aurel fontaine <ajfame11@gmail.com>
2020-06-20 23:35:41 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,27 +1,59 @@
 class PigLatinizer
 
-  def piglatinize(word)
-    return word if %w[and an in].include?(word) #one syllable exceptions
-    letters = word.split("")
-    letters.keep_if {|letter| letter != "."}
-    if letters.size > 1
-      until vowel?(letters[0]) 
-        letters << letters.shift
-      end
-      letters  << "ay"
+
+
+  def piglatinize(input_str)
+
+    input_str.split(' ').length == 1 ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
+
+  end
+
+
+
+  private
+
+
+
+  def consonant?(char)
+
+    !char.match(/[aAeEiIoOuU]/)
+
+  end
+
+
+
+  def piglatinize_word(word)
+
+    if !consonant?(word[0])
+
+      word += 'w'
+
+    elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
+
+      word = word.slice(3..-1) + word.slice(0, 3)
+
+    elsif consonant?(word[0]) && consonant?(word[1])
+
+      word = word.slice(2..-1) + word.slice(0, 2)
+
+    else
+
+      word = word.slice(1..-1) + word.slice(0)
+
     end
-    letters.join
+
+    word << 'ay'
+
   end
 
-  def to_pig_latin(text)
-    words = text.split(" ")
-    words.map! {|word| piglatinize(word)}
-    words.join(" ")
+
+
+  def piglatinize_sentence(sentence)
+
+    sentence.split.collect { |word| piglatinize_word(word) }.join(' ')
+
   end
 
-  def vowel?(letter)
-    letter.downcase
-    letter == "o" || letter == "e" || letter == "a" || letter == "i" || letter == "u"
-  end
+
 
 end
